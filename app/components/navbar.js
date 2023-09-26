d791c4037adf35c3a21af034684b0461b7fd1535
jaimin25 <jaiminchovatia25123@gmail.com>
2023-09-26 14:17:52 +0530

    UI changes, added landing page

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -3,22 +3,28 @@ import {
     faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <nav className="flex shadow-md h-[64px] items-center">
+        <nav className="flex shadow-md h-[64px] items-center backdrop-blur-sm sticky top-0">
             <div className="flex w-full justify-center items-center">
-                <div className="flex items-center p-2">
-                    <FontAwesomeIcon
-                        className="h-[28px] text-green-600"
-                        icon={faCartShopping}
-                    />
-                    <span className="h-full ml-1 text-lg font-black italic text-green-600 ">
-                        eShop
-                    </span>
-                </div>
+                <Link href="/">
+                    <div className="flex items-center p-2">
+                        <FontAwesomeIcon
+                            className="h-[28px] text-[#56c1d6]"
+                            icon={faCartShopping}
+                        />
+
+                        <span className="h-full ml-1 text-lg font-black italic text-[#56c1d6] ">
+                            eShop
+                        </span>
+                    </div>
+                </Link>
                 <ul className="flex gap-4 p-2 w-full justify-end">
-                    <li>Home</li>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
                     <li>Products</li>
                     <li>About</li>
                 </ul>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import NavBar from "@/app/components/navbar";
 import Image from "next/image";
-
+import img from "../public/img.jpg";
 export default function Home() {
     return (
-        <main>
+        <main className="h-full">
             <NavBar />
+            <div className="landing-cont flex p-2 h-full justify-center items-center">
+                <div className="flex-col justify-center w-full lg:w-1/2 md:w-1/2">
+                    <p className="p-2 font-black text-[48px]">
+                        <span className="text-[#56c1d6]">Welcome</span> to our{" "}
+                        <span className="text-[#56c1d6]">new online</span> store
+                        <span className="text-[#56c1d6]">.</span>
+                    </p>
+                    <p className="text-sm p-2">
+                        Sit do ipsum sit anim sunt reprehenderit id ut velit
+                        reprehenderit adipisicing nulla irure. Proident culpa
+                        sit nulla esse veniam ea nulla cillum.
+                    </p>
+                </div>
+                <Image
+                    src={img}
+                    className="w-[400px] h-[300px] rounded-lg hidden lg:block md:block"
+                    alt="img"
+                />
+            </div>
         </main>
     );
 }
