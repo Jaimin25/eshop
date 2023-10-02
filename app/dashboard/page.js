dd1dd8251b171df98d075dfc3d55a7718b8d63e7
jaimin25 <jaiminchovatia25123@gmail.com>
2023-10-02 21:47:49 +0530

    update

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -7,14 +7,19 @@ export default async function Dashboard() {
     const secret = process.env.protection_secret;
     let userData = {};
     const getUserDetails = async (e) => {
-        const res = await fetch(
-            `http://localhost:3000//api/user?email=${
-                user.email
-            }&secret=${encodeURIComponent(secret)}`
-        );
+        try {
+            const res = await fetch(
+                `http://localhost:3000//api/user?email=${
+                    user.email
+                }&secret=${encodeURIComponent(secret)}`
+            );
 
-        userData = await res.json();
-        return userData.result.user;
+            userData = await res.json();
+            return userData.result.user;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
     };
 
     userData = await getUserDetails();
