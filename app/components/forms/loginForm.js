aa436514a1d4ea872e7ae0a239dc1a0a9fcdb928
jaimin25 <jaiminchovatia25123@gmail.com>
2023-10-05 21:51:58 +0530

    address form added, and can update name in account details section

--- app/components/forms/loginForm.js
+++ app/components/forms/loginForm.js
@@ -61,39 +61,41 @@ export default function LoginForm() {
         <div className="flex flex-col bg-white p-4 px-8 gap-4 w-full lg:w-1/3 md:w-1/2">
             {loading ? <Loader /> : null}
             <p className="text-xl font-semibold">Login</p>
-            <Input
-                label="Email"
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-                label="Password"
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
+            <form>
+                <Input
+                    label="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    label="Password"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-            <Typography
-                variant="small"
-                className="flex justify-center">
-                Don&apos;t have an account?
                 <Typography
-                    as="a"
-                    href="/auth/signUp"
                     variant="small"
-                    color="blue-gray"
-                    className="ml-1 font-bold">
-                    Sign up
+                    className="flex justify-center">
+                    Don&apos;t have an account?
+                    <Typography
+                        as="a"
+                        href="/auth/signUp"
+                        variant="small"
+                        color="blue-gray"
+                        className="ml-1 font-bold">
+                        Sign up
+                    </Typography>
                 </Typography>
-            </Typography>
-            {error && (
-                <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-                    {error}
-                </span>
-            )}
-            <button
-                onClick={signInUser}
-                className="border px-8 py-2 hover:bg-[#00000020] transition">
-                Sign In
-            </button>
+                {error && (
+                    <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                        {error}
+                    </span>
+                )}
+                <button
+                    onClick={signInUser}
+                    className="border px-8 py-2 hover:bg-[#00000020] transition">
+                    Sign In
+                </button>
+            </form>
         </div>
     );
 }
--- app/components/pages/dashboard/addressComponent.js
+++ app/components/pages/dashboard/addressComponent.js
@@ -1,8 +1,49 @@
 export default function AddressLayout() {
     return (
         <div className="flex flex-col w-full p-2">
-            <p className="text-base font-bold p-2 text-[#262626]">Addresses</p>
+            <p className="text-base font-bold p-2 text-[#262626]">Address</p>
             <hr className="border-b-1 m-2" />
+            <p className="px-2 py-4 pb-1 text-sm">Address</p>
+            <input
+                className="text-xs outline-none p-3 border mx-2 rounded-sm"
+                type="text"
+                placeholder="Street, House No/Apartment No"
+            />
+            <p className="px-2 py-4 pb-1 text-sm">City</p>
+            <input
+                className="text-xs outline-none p-3 border mx-2"
+                type="text"
+                placeholder="City"
+            />
+            <div className="flex">
+                <div className="flex flex-col mr-2 flex-1">
+                    <p className="px-2 py-4 pb-1 text-sm">State</p>
+                    <input
+                        className="text-xs outline-none p-3 border mx-2"
+                        type="text"
+                        placeholder="State"
+                    />
+                </div>
+                <div className="flex flex-col flex-1">
+                    <p className="px-2 py-4 pb-1 text-sm">Country</p>
+                    <input
+                        className="text-xs outline-none p-3 border mx-2"
+                        type="text"
+                        placeholder="Country"
+                    />
+                </div>
+            </div>
+            <p className="px-2 py-4 pb-1 text-sm">Zipcode</p>
+            <input
+                className="text-xs outline-none p-3 border mx-2"
+                type="text"
+                placeholder="Zipcode"
+            />
+            <div className="p-2 mt-2 text-sm text-[#323232]">
+                <button className="p-2 border outline-none">
+                    Save Changes
+                </button>
+            </div>
         </div>
     );
 }
