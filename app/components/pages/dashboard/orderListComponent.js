1872c70c77bdc8a12aa51b9165a0effe3507e53f
jaimin25 <jaiminchovatia25123@gmail.com>
2023-10-15 21:16:30 +0530

    fixing dashboard order page

--- app/components/pages/dashboard/orderListComponent.js
+++ app/components/pages/dashboard/orderListComponent.js
@@ -1,19 +1,56 @@
-import Link from "next/link";
+"use client";
 
-export default function OrderListComponent({ secretKey, ordersList }) {
+import { base_url } from "@/app/lib/baseUrl";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Loader from "../../ui/loader";
+
+export default function OrderListComponent({ secretKey }) {
+    const { data: session } = useSession();
+    const sessionUser = session ? session.user : null;
+    const [loading, setLoading] = useState(true);
+    const [ordersList, setOrdersList] = useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+        getUserOrders();
+    }, [sessionUser]);
+
+    const getUserOrders = async () => {
+        try {
+            const data = await fetch(
+                `${base_url}/api/account/user/order?userId=${
+                    sessionUser.userid
+                }&secretKey=${encodeURIComponent(secretKey)}`
+            );
+
+            if (data.ok) {
+                const res = await data.json();
+                setOrdersList(res.result);
+                setLoading(false);
+            }
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
         <div className="w-full p-2">
             <p className="text-base font-bold p-2 text-[#262626]">
                 Your Orders
             </p>
-
+            {loading ? <Loader /> : null}
             <hr className="border-b border-[#e4e6eb]" />
+
             {ordersList.length > 0 ? (
                 <div className="flex flex-col">
                     <p className="p-2">{ordersList.length} orders</p>
                     <div>
                         {ordersList.map((item, index) => (
-                            <Link href={"/order/" + item._id}>
+                            <Link
+                                href={"/order/" + item._id}
+                                key={index}>
                                 <div className="flex flex-col border border-[#e4e6eb] p-2 rounded shadow gap-1 hover:bg-[#00000008] hover:transition cursor-pointer text-sm">
                                     <p>
                                         Order{" "}
--- app/dashboard/orders/page.js
+++ app/dashboard/orders/page.js
@@ -5,24 +5,6 @@ import { getServerSession } from "next-auth";
 
 export default async function OrderListPage() {
     const secretKey = process.env.protection_secret;
-    const session = await getServerSession(authOptions);
-    const userid = session.user.userid.toString();
 
-    async function getUserOrder() {
-        const data = await fetch(
-            `${base_url}/api/account/user/order?userId=${userid}&secretKey=${encodeURIComponent(
-                secretKey
-            )}`
-        );
-        const res = await data.json();
-        return res.result;
-    }
-    const ordersList = await getUserOrder();
-
-    return (
-        <OrderListComponent
-            secretKey={secretKey}
-            ordersList={ordersList}
-        />
-    );
+    return <OrderListComponent secretKey={secretKey} />;
 }
